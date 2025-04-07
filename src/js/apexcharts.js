5048758f294a134b72a0bd7b6898dfbd91aa020d
Therese Nevander <thne1900@student.miun.se>
2025-04-07 09:12:09 +0200

    Gjort första diagrammet.

--- src/js/apexcharts.js
+++ src/js/apexcharts.js
@@ -1,26 +1,64 @@
 /*Javascript ApexCharts*/
 "use strict";
 
+//Data från extern url med Fetch-api med async/await.
+let data=[];
+
+window.onload=()=>{
+    fetchData();
+}
+
+async function fetchData() {
+
+    try {
+        const response=await fetch('https://studenter.miun.se/~mallar/dt211g/');
+        if(!response.ok){
+            throw new Error("Fel vid inhämtningen av datan");
+        }
+
+        data = await response.json();
+        displayData(data);
+        
+
+    }catch (error) {
+        console.error("Ett fel uppstod", error);
+        throw error;
+    }
+    console.table(data);
+}
+ function displayData(data) {
+
+    let filteredCourses=data.filter((item)=>item.type==="Kurs");
+    console.log(filteredCourses);
+
+    let topCourses=filteredCourses.sort((a, b)=>b.applicantsTotal-a.applicantsTotal).slice(0,6);
+    console.table(topCourses);
+
+    let outputCourses=topCourses.map(item=>item.name);
+    let totalApplicants=topCourses.map(item=>item.applicantsTotal);
+
     let options= {
         chart: {
             type:"bar"
         },
         series:[{
             name:"Totalt sökantal",
-            data: [1,2,3,4,5,6]
+            data:totalApplicants
         }],
         xaxis: {
-            categories:[1,2,3,4,5,6]
+            categories:outputCourses
         },
         title: {
             text:"De 6 mest sökta kurserna på Mittuniversitetet HT24",
             align: "center",
             style: {
-                fontSize:"16px",
+                fontSize:"14px",
                 color: "white",
             }
         }
-    }
-    let chart= new ApexCharts (document.querySelector("#chart"), options)
-    chart.render()
+    };
+    let chart= new ApexCharts (document.querySelector("#chart"), options);
+    chart.render();
+   
+}
 
