a1bb1e74e2d80932ab1605ace0e17bd73d3fec8d
Therese Nevander <thne1900@student.miun.se>
2025-04-22 21:28:09 +0200

    Kommenterat i map.js filen och uppdaterat md-filen.

--- README.md
+++ README.md
@@ -15,4 +15,13 @@
 - Skapade en ny js-fil, map.js, där javascriptkoden innehållande karta och dess funktioner finns. 
 - I map.js-filen skrevs kod för att få ut en bild på en karta och som gör att vid inskriven stad i inputfältet, ska staden visas på kartan. Lade till i funktinen att koordinaterna ska visas tillsammans med den visade staden.
 - Placering och storlek har därefter gjorts på diagrammen och kartan i scss-filerna. 
- 
+- Javascript-filerna har dokumenterats med JSDoc.
+
+## Av Thne1900
+### Kurs: Frontend-baserad webbutveckling
+
+#### Källor:
+- Diagram: https://apexcharts.com/
+- Karta: https://www.openstreetmap.org/#map=14/59.33067/18.05200, https://leafletjs.com/examples/quick-start/
+        https://nominatim.openstreetmap.org/ui/search.html.
+
--- src/js/map.js
+++ src/js/map.js
@@ -1,48 +1,73 @@
+/*Javascript-map*/
+
 "use strict";
+/**
+ * @type {L.map} Infogar en karta med variabeln: map, med utgångspunkt i Stockholm, Sverige och koordinater och inzoomningsgrad. 
+ */
+let map = L.map("map").setView([59.32512, 18.07109], 5);
 
-let map = L.map('map').setView([59.32512, 18.07109], 5);
-
+/**
+ * @type {L.tileLayer} Lägger på ett tileLayer från OpenStreetmap med en maxzoom. 
+ */
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom:19,
+    maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+/**
+ * @type {L.marker} Skapat en variabel för att markera på kartan.
+ */
 let marker;
+
+/**
+ * @type {HTMLElement} Vid klick (händelsehanterare) på knappen anropas funktionen (searchCity).
+ */
 document.getElementById("button-search").addEventListener("click", searchCity);
 
-function searchCity(){
+/**
+ * @async visar att en asynkron funktion används.
+ * @function searchCity Vid inmatning i inputfältet ska den sökta staden visas på kartan med en markering. 
+ * @throws {Error} Om det uppstår fel vid den inhämtade datan, blir det felmeddelanden.
+ * @example Då en besökare klickar på knappen SÖK STAD ("button-search"), kommer funktionen searchCity att anropas,
+  som kommer visa den sökta staden på kartan med dess koordinater.
+ */
+async function searchCity() {
 
-        let city=document.getElementById("search").value;
-        let url=`https://nominatim.openstreetmap.org/search?format=json&q=${city}`;
-    
-        fetch(url)
-        .then(response=>{
-            if(!response.ok){
-                throw new Error("Det blev fel vid inhämtningen");
+    let city = document.getElementById("search").value;
+    let url = `https://nominatim.openstreetmap.org/search?format=json&q=${city}`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Det blev ett fel vid inhämtningen");
+        }
+
+        const data = await
+            response.json();
+
+        if (data.length > 0) {
+            let lati = data[0].lat;
+            let long = data[0].lon;
+
+            map.setView([lati, long], 13);
+
+            if (marker) {
+                marker.setLatLng([lati, long]);
+
+            } else {
+                marker = L.marker([lati, long]).addTo(map);
             }
-            return response.json();
-        })
-        .then(data=>{
-            if(data.length>0){
-                let lat=data[0].lat;
-                let lon=data[0].lon;
-    
-                map.setView([lat, lon], 13);
-    
-                if (marker){
-                    marker.setLatLng([lat, lon]);
-                    }else{
-                        marker=L.marker([lat, lon]).addTo(map);
-                    }
 
-                    marker.bindPopup(`Här är ${city}! (Lat: ${lat} och Lon: ${lon})`).openPopup();
-                }else {
-                    alert("Din sökning av önskad stad kunde inte hittas, prova med en annan!")
+            marker.bindPopup(`Här är ${city}! (Lat: ${lati} och Lon: ${long})`).openPopup();
+        } else {
+            alert("Din sökning av önskad stad kunde inte hittas, prova med en annan!")
+        }
 
-                }
-            })
-            .catch(error=>{
-                console.error("Ett fel uppstod", error);
+    } catch (error) {
+        console.error("Ett fel uppstod", error);
 
-            }
-        )};
+    }
+}
+
+
