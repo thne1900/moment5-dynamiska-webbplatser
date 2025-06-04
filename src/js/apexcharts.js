252e2ae80de2e97527dfa493add969da2a325817
Therese Nevander <thne1900@student.miun.se>
2025-06-04 13:50:33 +0200

    Ändringar i jsdoc

--- src/js/apexcharts.js
+++ src/js/apexcharts.js
@@ -2,12 +2,12 @@
 "use strict";
 
 /**
- * @type {Array} En tom array som har som syfte att fyllas med data. 
+ * @type {Array}  - En tom array som har som syfte att fyllas med data.
  */
 let data=[];
 
 /**
- * @function window.onload körs vid sidinladdning, som med anrop(fetchData) hämtar in data. 
+ * @function window.onload - Körs vid sidinladdning, som med anrop(fetchData) hämtar in data. 
  */
 window.onload=()=>{
     fetchData();
@@ -16,11 +16,11 @@ window.onload=()=>{
 /**
  * Ska inhämta data från en extern källa (url) och visa i diagram.
  * 
- * @async Visar att det är en asynkron funktion.
- * @function fetchData gör anrop till url för att komma åt den innehållande datan.
- * @function displayData anropas för att få ut denna funktionens efterfrågade data i ett stapeldiagram.
- * @function displayDataAgain anropas för att få ut den funktionens begärda data i ett cirkeldiagram. 
- * @throws {Error}- ger felmeddelande vid problem vid inhämtning av datan. 
+ * @async - Asynkron funktion.
+ * @function fetchData - Gör anrop till url för att komma åt den innehållande datan.
+ * @function displayDataTopSix - Anropas för att få ut denna funktionens efterfrågade data i ett stapeldiagram.
+ * @function displayDataTopFive - Anropas för att få ut den funktionens begärda data i ett cirkeldiagram. 
+ * @throws {Error} - Ger felmeddelande vid problem vid inhämtning av datan. 
  */
 async function fetchData() {
 
@@ -39,20 +39,21 @@ async function fetchData() {
         console.error("Ett fel uppstod", error);
         throw error;
     }
-    console.table(data);
+    //console.table(data);
 }
 
 /**
- * @function displayDataTopSix Filtrerar och sorterar datan.
- * @param {Array} data Inhämtad data som ska hanteras för att sedan visas i ett stapeldiagram.
+ * Hanterar datan så att den kommer ut i ett stapeldiagram.
+ * @function displayDataTopSix - Filtrerar och sorterar datan.
+ * @param {Array} data - Inhämtad data som ska hanteras för att visas i ett stapeldiagrammet.
  */
  function displayDataTopSix(data) {
 
     let filteredCourses=data.filter((item)=>item.type==="Kurs");
-    console.log(filteredCourses);
+    //console.log(filteredCourses);
 
     let topCourses=filteredCourses.sort((a, b)=>b.applicantsTotal-a.applicantsTotal).slice(0,6);
-    console.table(topCourses);
+    //console.table(topCourses);
 
     let outputCourses=topCourses.map(item=>item.name);
     let totalApplicants=topCourses.map(item=>parseInt(item.applicantsTotal, 10));
@@ -83,23 +84,23 @@ async function fetchData() {
 }
 
 /**
- * @function displayDataTopFive Filtrerar och sorterar datan.
- * @param {Array} data Inhämtad data som ska hanteras för att sedan visas i ett cirkeldiagram.
+ * Hantera datan så att den visas i ett cirkeldiagram. 
+ * @function displayDataTopFive - Filtrerar och sorterar datan.
+ * @param {Array} data - Inhämtad data som ska hanteras för att visas i ett cirkeldiagrammet.
  */
 function displayDataTopFive(data) {
 
     let filteredProgram=data.filter((item)=>item.type==="Program");
-    console.log(filteredProgram);
+    //console.log(filteredProgram);
 
     let topProgram=filteredProgram.sort((a, b)=>b.applicantsTotal-a.applicantsTotal).slice(0,5);
-    console.table(topProgram);
+    //console.table(topProgram);
 
     let outputProgram=topProgram.map(item=>item.name);
     let tApplicants=topProgram.map(item=>parseInt(item.applicantsTotal,10));
-    console.log(tApplicants);
+    //console.log(tApplicants);
 
     let options= {
-        
         chart: {
             type:"pie",
         },
